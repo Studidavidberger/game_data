0ab4e60ae7730eed624fea1a6774d1b9f1a1013a
davidos_studi <david.ber0169@gmail.com>
2025-06-19 03:54:14 +0200

    Ajout de beaucoup de ressources et en code, ajout de la variable d'envrionnement globale world, ajout d'une fonctionnalité d'input de l'username avec fleche du haut et du bas.

--- main.lua
+++ main.lua
@@ -1,65 +1,102 @@
--- main.lua contain the loading screen game 
+-- main.lua contain the loading screensize game 
 
--- display screen
+--import CoZ modules
+
+
+-- display screensize
 love.window.setMode( 1280, 720, {fullscreen = false, vsync = true, resizable = true } )
 
 -- socket.sleep
 socket = require'socket'
 
--- local
-local video = love.graphics.newVideo('opening.ogv')
-local username = ""
+-- globals objects
+cursor = love.graphics.newImage('assets/world/images/cursor.png')
 
--- global
-bg = love.graphics.newImage('assets/world/images/bg/bg_menu.jpg')
 player = {
-  score = 0,
-  character = 0,
-  name = "Gamiron",
-  online_score = 0,
-  }
+    score = 0,
+    character = 0,
+    username = "",
+    online_score = 0
+}
 
+world = {
+  -- images
+  video = love.graphics.newVideo('opening.ogv'),
+  bgm = love.audio.newSource('assets/sounds/bgm_menu.ogg', "stream"),
+  bg = love.graphics.newImage('assets/world/images/bg/bg_day.jpg'),
+  screen = "",
+  
+  -- mouse
+  mouse = love.graphics.newImage('assets/world/images/cursor.png'),
+  mouse_X = love.mouse.getX(),
+  mouse_Y = love.mouse.getY(),
+  
+  -- Langue
+  language = "fr",
+  titles = "5",
+  txt_4 = "4",
+  txt_3 = "3",
+  txt_2 = "2"
+}
+  
 -- Everything that need to be refresh and is constant
 function love.update()
   
-  -- Get size of the window.
-  screen = {
+  screensize = {
+    -- GET screensize SIZE
+    wallpaper = love.graphics.newImage('assets/world/images/bg/bg_day.jpg'),
     X = love.graphics.getWidth(),
     Y = love.graphics.getHeight(),
-    state = "",
-  }
-  
-  -- load cursor.png
-  cursor = love.graphics.newImage('assets/world/images/cursor.png')
-  
-  -- set world data
-  world = {
-    bg_x = screen.X / bg:getWidth(),
-    bg_y = screen.Y / bg:getHeight(),
-    }
-
+    ratio_X = love.graphics.getWidth()/world.bg:getWidth(),
+    ratio_Y = love.graphics.getHeight()/world.bg:getHeight(),
+}
+print(screensize.ratio_X, screensize.ratio_Y)
 end
 
 
 function ft_intro()
   
   --debug
-  screen = "intro"
-  print("screen : ", screen)
+  world.screen = "intro"
+  print("screen : ", world.screen)
   
   -- Play the opening one time at start
-  video:play()
+  world.video:play()
   
   
   -- Display the screen and text intro
   function love.draw()
-    local bg_width = screen.X / bg:getWidth()
-    local bg_height = screen.Y / bg:getHeight()
-    love.graphics.draw(video,0,0,r, bg_width, bg_height)
-    love.graphics.print("Press 'Space Bar'", screen.X/2-20, screen.Y/2,r,bg_width*5,bg_height*5)
-    love.graphics.print("'P' to pause", screen.X/4-20, screen.Y/4,r,bg_width*5,bg_height*5)
-    love.graphics.print("'R' to rewind", screen.X/8-20, screen.Y/1.5,r,bg_width*5,bg_height*5)
+    love.graphics.draw(world.video,0,0,r, screensize.ratio_X, screensize.ratio_Y)
+    love.graphics.print("Press 'Space Bar'", screensize.X/2-20, screensize.Y/2,r,5,5)
+    love.graphics.print("'P' to pause", screensize.X/4-20, screensize.Y/4,r,5,5)
+    love.graphics.print("'R' to rewind", screensize.X/8-20, screensize.Y/1.5,r,5,5)
+  
+  function love.keypressed(key)
+  if key == 'p' then
+    if (world.video:isPlaying()) then
+      world.video:pause()
+      else
+    world.video:play()
   end
+  end
+
+  if key == 'escape' then
+    love.graphics.print("Quitter vraiment ?!.. " .. world.screen,  screensize.X/2,screensize.Y/2,r,3,3)
+    print("Quitter vraiment ?!.. " .. world.screen)
+    --love.event.quit('quit')
+  end
+
+  if key == 'r' then
+    video:rewind()
+  end
+  
+  if key == 'space' then
+  ft_menu()
+  print("screensize : menu")
+end
+end
+    end
+
 end
 -- init the game start by the intro
 ft_intro()
@@ -73,92 +110,85 @@ function ft_menu()
   --load bgm_menu.ogg
   bgm = love.audio.newSource('assets/sounds/bgm_menu.ogg', "stream")
 
-  local btn_scoreX, btn_scoreY, btn_scoreH, btn_scoreW = screen.X/3, 20, bg_width/2, bg_height/2
-  local btn_score = love.graphics.newImage('assets/world/images/textures/btn_score.jpg')
-  video:pause()
+  local btn_playX, btn_playY, btn_playH, btn_playW = screensize.X/3, 20, bg:getWidth()/2, bg:getHeight()/2
+  world.video:pause()
   love.audio.play(bgm)
 
   function love.draw()
   
-
-  
-  local btn_score = {
-    sprite = love.graphics.newImage('assets/world/images/textures/btn_score.jpg'),
-    X = screen.X/2-190,
-    Y = screen.Y/10,
+    
+  local btn_pseudo = {
+    sprite = love.graphics.newImage('assets/world/images/items/btn_pseudo.png'),
+    X = screensize.X/2-190,
+    Y = screensize.Y/2,
     right = 0,
     down = 0,
-    ratio = .3
+    ratio = 1
   }
   
-  btn_score.right = btn_score.X + btn_score.sprite:getWidth() * btn_score.ratio
-  btn_score.down = btn_score.Y + btn_score.sprite:getHeight() * btn_score.ratio
-      
-
-      
-      local mouse = {
-    sprite = love.graphics.newImage('assets/world/images/cursor.png'),
-    X = love.mouse.getX(),
-    Y = love.mouse.getY()
+  local btn_play = {
+    sprite = love.graphics.newImage('assets/world/images/items/btn_play.png'),
+    X = screensize.X/2-190,
+    Y = screensize.Y/10,
+    right = 0,
+    down = 0,
+    ratio = 1
   }
-    love.graphics.draw(bg,0, 0,r, bg_width, bg_height)
-    love.graphics.print("BUTTON : X " .. btn_score.X, 100,100)
-    love.graphics.print("BUTTON : Y " .. btn_score.Y,100,120)
-    love.graphics.print("BUTTON DOWN " .. btn_score.down,100,160)
-    love.graphics.print("BUTTON RIGHT " .. btn_score.right,100,140)
-    love.graphics.print("MOUSE : X " .. mouse.X,100,180)
-    love.graphics.print("MOUSE : Y " .. mouse.Y,100,200)
-    
-    
-    love.graphics.print("TEST : Y " ,screen.X/2,250)
-  if ( mouse.X >= btn_score.X and mouse.X <= btn_score.right and
-  mouse.Y >= btn_score.Y and mouse.Y <= btn_score.down ) then
   
-    btn_score.sprite = love.graphics.newImage('assets/world/images/textures/btn_score_light.jpg')
-  love.graphics.draw(btn_score.sprite, btn_score.X, btn_score.Y,r, btn_score.ratio,btn_score.ratio)
+  btn_play.right = btn_play.X + btn_play.sprite:getWidth() * btn_play.ratio
+  btn_play.down = btn_play.Y + btn_play.sprite:getHeight() * btn_play.ratio
+        
+  if ( world.mouse_X >= btn_play.X and world.mouse_X <= btn_play.right and
+    world.mouse_Y >= btn_play.Y and world.mouse_Y <= btn_play.down ) then
+  
+  btn_play.sprite = love.graphics.newImage('assets/world/images/items/btn_play_light.png')
+  love.graphics.draw(btn_play.sprite, btn_play.X, btn_play.Y,r, btn_play.ratio,btn_play.ratio)
     
     --[[
     print("\nMOUSE: X " .. mouse.X .. " Y " .. mouse.Y)
-    print("SCREEN: X " .. screen.X .. " Y " .. screen.X)
-    print("BUTTON: X " .. btn_score.X .. " Y " .. btn_score.Y)
-    ]]
+    print("screensize: X " .. screensize.X .. " Y " .. screensize.X)
+    print("BUTTON: X " .. btn_play.X .. " Y " .. btn_play.Y)
+    --]]
+  
   else
-  love.graphics.draw(btn_score.sprite, btn_score.X, btn_score.Y,r, btn_score.ratio,btn_score.ratio)
 end
-    love.graphics.draw(mouse.sprite, love.mouse.getX(), love.mouse.getY())
-    love.mouse.setVisible(false)
+  
+  love.graphics.draw(world.bg,0, 0,r, world.ratio_X, world.ratio_Y)
+  love.graphics.draw(btn_play.sprite, btn_play.X, btn_play.Y,r, btn_play.ratio,btn_play.ratio)
+  love.graphics.draw(btn_pseudo.sprite, btn_pseudo.X, btn_pseudo.Y,r, btn_pseudo.ratio,btn_pseudo.ratio)
+  love.graphics.print(player.username, screensize.X/2,250)
+  love.graphics.draw(world.mouse, love.mouse.getX(), love.mouse.getY())
+  love.graphics.draw(world.mouse, love.mouse.getX(), love.mouse.getY())
+  love.mouse.setVisible(false)
   
 end
 
+    function love.keypressed(key)
+      alphabet = {"a","b","c","d","e","f","g","h","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y",1,2,3,4,5,6,7,8,9,"-","_","@"}
+      player.username = alphabet[i]
+      if key == "up" then
+          i = i + 1
+      end      
+      if key == "down" then
+          i = i - 1
+      end
+      if key == "space" then
+        player.username = player.username
+      end
+      
+  end
+
+end
+--
 
   function love.keypressed(key)
-  
     if key == 'escape' then
-      love.event.quit('quit')
+      love.graphics.print("Quitter vraiment ?!.. " .. world.screen,  screensize.X/2,screensize.Y/2,r,3,3)
+      print("Quitter vraiment ?!.. " .. world.screen)
+      --love.event.quit('quit')
     end
-    username = username .. key
   end
   
-end
---
-function love.keypressed(key)
-  
-  if key == 'p' then
-    if (video:isPlaying()) then
-      video:pause()
-      else
-    video:play()
-  end
-end
-
-  if key == 'r' then
-    video:rewind()
-  end
-  
-  if key == 'space' then
-  ft_menu()
-  print("screen : menu")
-end
-end
 --
 io.stdout:setvbuf("no")
+	
