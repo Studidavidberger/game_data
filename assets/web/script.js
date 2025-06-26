3c4615b12a96e2775411f921cef7aae177ec7a41
dabergery <axiomcity@ik.me>
2025-06-26 16:42:26 +0200

    web version

---
+++ assets/web/script.js
@@ -0,0 +1,12 @@
+// DECLARE BTNS
+const btn_start = document.getElementById('btn_game');
+const btn_game = document.getElementById('btn_game');
+const view = document.getElementById('view');
+
+// LOGIC OF THE WEBSITE BTNS
+
+btn_start.addEventListener('click', () => {
+
+    if view.style.display === 'block':
+
+})
